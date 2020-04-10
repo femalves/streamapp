64ebf885f908e53509cd47dddaae4b001bd61a60
Fernanda Alves <fernanda__m@hotmail.com>
2020-04-10 19:32:57 -0300

    Updates

---
+++ precache-manifest.f2629db488f0ef09c026e091e3a5c546.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e4551fec5b30f9d38ad9bc2a4909af7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53ca120c74dce3627f3f",
+    "url": "/static/js/2.8093f5ca.chunk.js"
+  },
+  {
+    "revision": "c0f4aa119942e5c1f85dd4acb785bdb8",
+    "url": "/static/js/2.8093f5ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a642ea26265aa7071074",
+    "url": "/static/js/main.8e8242e6.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
